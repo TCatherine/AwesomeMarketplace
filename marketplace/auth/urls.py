64293878291f1ca49de1b5b0fc22140a78451afc
Dmitry Vodyanoy <dmitry.vodyanoy@gmail.com>
2022-03-12 22:15:44 +0300

    API READY: create user, get bearer token, refresh bearer token.

--- marketplace/auth/urls.py
+++ marketplace/auth/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+
+from .views import RegisterView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('auth.urls')),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
