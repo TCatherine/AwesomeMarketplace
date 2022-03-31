a7032087eb2d5ab190277d45604ded1e1622849b
Dmitry Vodyanoy <dmitry.vodyanoy@gmail.com>
2022-03-31 15:07:44 +0300

    Docker debugged. Transactions passed. Todo todo.

--- marketplace/marketplace/blockchain/tasks.py
+++ marketplace/marketplace/blockchain/tasks.py
@@ -9,29 +9,30 @@ from .models import Transaction, ConfirmedTransaction
 from ..market.models import SellableObject
 from ..market.models import CustomUser as User
 from decimal import Decimal
+from django.conf import settings
 
 @app.task
-def create_transaction(buyer, seller, amount, item):
+def create_transaction(buyer, seller, amount, item_id):
     buyer = User.objects.get(pk=buyer)
     seller = User.objects.get(pk=seller)
     amount = Decimal(amount)
-    item = SellableObject.objects.get(pk=item)
-    ta = Transaction(buyer=buyer, seller=seller, amount=amount, item=item)
+    item_id = SellableObject.objects.get(pk=item_id)
+    ta = Transaction(buyer=buyer, seller=seller, amount=amount, item=item_id)
     ta.save()
     try:  # try to get block with transaction
         r = requests.post(
-            "http://localhost:5000/transactions/new",
+            "http://" + settings.BLOCKCHAIN_HOST + ":5000/transactions/new",
             json={
                 "sender": buyer.username,
                 "recipient": seller.username,
                 "amount": str(amount),
-                "item": str(item.id)
+                "item": str(item_id.id)
             }
         )
         if r.status_code != 201:
             raise ValueError("New transaction creating error response")
 
-        r = requests.get("http://localhost:5000/mine")
+        r = requests.get("http://" + settings.BLOCKCHAIN_HOST + ":5000/mine")
         if r.status_code != 200:
             raise ValueError("Mining block error")
     except Exception as e:
@@ -42,13 +43,11 @@ def create_transaction(buyer, seller, amount, item):
 
 
 @app.task
-def confirm_transaction(result_ta_id, buyer, seller, amount, item):
-    ta = Transaction.objects.get(pk=result_ta_id)
+def confirm_transaction(ta_id):
+    ta = Transaction.objects.get(pk=ta_id)
+    item = SellableObject.objects.get(pk=ta.item.id)
+    item.owner = ta.buyer
+    cta = ConfirmedTransaction(buyer=ta.buyer.id, seller=ta.seller.id, amount=ta.amount, item=ta.item)
+    item.save(update_fields=['owner'])
+    cta.save()
     ta.delete()
-    buyer = User.objects.get(pk=buyer)
-    seller = User.objects.get(pk=seller)
-    amount = Decimal(amount)
-    item = SellableObject.objects.get(pk=item)
-    item.owner = buyer
-    ta = ConfirmedTransaction(buyer=buyer, seller=seller, amount=amount, item=item)
-    ta.save()
--- marketplace/marketplace/blockchain/views.py
+++ marketplace/marketplace/blockchain/views.py
@@ -1,8 +1,10 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .tasks import create_transaction, confirm_transaction
 from django.http import HttpResponse
 from ..market.models import CustomUser as User
 from ..market.models import SellableObject
+from django.conf import settings
 
 import requests
 
@@ -11,7 +13,7 @@ class GetChainView(APIView):
 
     def get(self, request):
         r = requests.get(
-            "http://localhost:5000/chain"
+            "http://" + settings.BLOCKCHAIN_HOST + ":5000/chain"
         )
         if r.status_code != 200:
             raise ValueError("Error occures while getting chain")
@@ -21,16 +23,19 @@ class GetChainView(APIView):
 
 # TEST
 class Test(APIView):
+    permission_classes = (IsAuthenticated,)
     # TODO: add IsAuthenticated
     # TODO: check user money
     def post(self, request):
         # creating sellable object with apply async and check callback works
         buyer = request.user.id
         # user = User.objects.get(pk=request.data["seller_id"])
-        seller = request.user.id
-        amount = request.data["amount"]
-        item = request.data["item_id"]
-
-        create_transaction.apply_async((buyer, seller, amount, item),
-                                       link=confirm_transaction.s(buyer, seller, amount, item))
+        item_id = request.data["item_id"]
+        item = SellableObject.objects.get(pk=item_id)
+        seller = item.owner.id
+        amount = item.price
+        # TODO: check if user has enougth money to do operation
+        # TODO: check if item is_sale and return 404 if not
+        create_transaction.apply_async((buyer, seller, amount, item_id),
+                                       link=confirm_transaction.s())
         return HttpResponse("Transaction initiated", content_type="text/plain")
